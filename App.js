b544382b602e6c254a40d0db2c68b349f4efa244
Ehsan Mohammadi <ehsanx64@gmail.com>
2021-03-02 18:54:28 +0330

    WIP: Build is broken

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Text } from 'react-native';
+import { Text, Button, View } from "react-native";
 import AppLoading from 'expo-app-loading';
 // import {
 //     Container, Content, Title, Body,
@@ -7,32 +7,90 @@ import AppLoading from 'expo-app-loading';
 // } from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
+import _ from 'lodash';
 
-import Home from './src/screens/Home/Home';
+import 'react-native-gesture-handler';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isReady: false,
-        };
-    }
 
-    async componentDidMount() {
-        await Font.loadAsync({
-            Roboto: require('native-base/Fonts/Roboto.ttf'),
-            Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-            ...Ionicons.font,
-        });
-        this.setState({ isReady: true });
-    }
+// import Sidebar from './src/screens/Sidebar';
+// import Home from './src/screens/Home/Home';
 
-    render() {
-        if (!this.state.isReady) {
-            return <AppLoading />;
-        }
+function AboutScreen({route, navigation }) { 
+    let referrer = '';
+    let aboutType = '';
 
-        return <Home />;
-    }
+    if (route.params !== undefined) {
+        referrer = _.get(route.params, 'referrer', 'none');
+        aboutType = _.get(route.params, 'aboutType', 'Not specified.')
+    } 
+
+    return (
+        <View>
+            <Text>Welcome to About screen</Text>
+            <Text>You are comming from: {referrer}</Text>
+            <Text>About type: {aboutType}</Text>
+
+            <Button title="Go to Contact Page" onPress={e => {
+                navigation.navigate('Contact', {
+                    contactMethod: 'email',
+                    referrer: 'about'
+                });
+            }} />
+        </View>
+    )
 }
 
+function ContactScreen({route, navigation }) {
+    let referrer = '';
+    let contactMethod = '';
+
+    if (route.params !== undefined) {
+        referrer = _.get(route.params, 'referrer', 'none');
+        contactMethod = _.get(route.params, 'contactMethod', 'Not specified.')
+    } 
+
+    return (
+        <View>
+            <Text>Welcome to Contact screen</Text>
+            <Text>You are comming from: {referrer}</Text>
+            <Text>Contact method: {contactMethod}</Text>
+            <Button title="Go to About Page" onPress={e => {
+                navigation.navigate('About', {
+                    aboutType: 'company',
+                    referrer: 'contact'
+                });
+            }} />
+        </View>
+    )
+}
+
+const Stack = createStackNavigator();
+
+// const Drawer = createDrawerNavigator();
+
+// export default App = () => {
+//     return (
+//         <NavigationContainer>
+//             <Drawer.Navigator initialRouteName="About">
+//                 <Drawer.Screen name="About" component={AboutScreen} />
+//                 <Drawer.Screen name="Contact" component={ContactScreen} />
+//             </Drawer.Navigator>
+//         </NavigationContainer>
+//     )
+// }
+
+
+export default App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                {/* <Stack.Screen name="Home" component={Home} /> */}
+                <Stack.Screen name="About" component={AboutScreen} />
+                <Stack.Screen name="Contact" component={ContactScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
